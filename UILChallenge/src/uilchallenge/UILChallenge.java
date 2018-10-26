f1ba8d21e508a626f848eca52d8f11e19dc39a3d
brighale <brighale@011-4303-S25SV.students.cisd.conroeisd.loc>
2018-10-26 08:59:21 -0500

    Did the UIL Challenge!

--- UILChallenge/src/uilchallenge/UILChallenge.java
+++ UILChallenge/src/uilchallenge/UILChallenge.java
@@ -5,7 +5,11 @@
  */
 package uilchallenge;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import static java.lang.System.out;
+import java.util.Scanner;
 
 /**
  *
@@ -16,9 +20,40 @@ public class UILChallenge {
      /**
       * @param args the command line arguments
       */
-     public static void main(String[] args) {
+     public static void main(String[] args) throws FileNotFoundException {
           // TODO code application logic here
-          out.println("Test");
+        String numberData;
+        
+        Scanner scan = new Scanner(new File("numbers.txt"));
+        int counter = scan.nextInt();
+        int num;
+        int num2;
+        int a;
+        int b;
+        
+        for(int i = 0; i < counter; i++){
+           num = scan.nextInt();
+           num2 = scan.nextInt();
+            
+            if(num > num2){
+                a = num;
+                b = num2;
+                
+            }else{
+                b = num;
+                a = num2;
+            }
+            System.out.printf("%d ", a + b);
+            System.out.printf("%d ", Math.abs(a - b));
+            System.out.printf("%d ", a * b);
+            System.out.printf("%.2f \n",(double)a / (double)b);
+            
+          
+        
+          }
+          
+           
+
      }
      
 }
